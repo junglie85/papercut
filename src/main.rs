0b8e37800a2f77ad0ffc65ebefa7fe020fec114d
Ashley Morton-Bye <ashley.morton.bye@gmail.com>
2022-10-10 14:30:13 +0100

    Add editor UI

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use editor::Editor;
 use flexi_logger::Logger;
-use pixels::{Pixels, SurfaceTexture};
+use log::error;
+use pixels::{PixelsBuilder, SurfaceTexture};
 use winit::{
     dpi::LogicalSize,
     event::Event,
@@ -7,6 +9,8 @@ use winit::{
     window::WindowBuilder,
 };
 use winit_input_helper::WinitInputHelper;
+
+mod editor;
 
 const WIDTH: u32 = 1024;
 const HEIGHT: u32 = 768;
@@ -21,33 +25,29 @@ fn main() -> anyhow::Result<()> {
         WindowBuilder::new()
             .with_title("Papercut")
             .with_inner_size(size)
+            .with_visible(false)
             .build(&event_loop)?
     };
 
     let mut input = WinitInputHelper::new();
 
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    let (mut pixels, mut editor) = {
+        let size = window.inner_size();
+        let scale_factor = window.scale_factor() as f32;
+        let surface_texture = SurfaceTexture::new(size.width, size.height, &window);
+        let pixels =
+            pollster::block_on(PixelsBuilder::new(WIDTH, HEIGHT, surface_texture).build_async())?;
+        let editor = Editor::new(&event_loop, size.width, size.height, scale_factor, &pixels);
+
+        (pixels, editor)
     };
 
     let app = App::default();
 
+    window.set_visible(true);
     let mut destroying = false;
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
-
-        if let Event::RedrawRequested(window_id) = event {
-            if !destroying && window_id == window.id() {
-                app.render(pixels.get_frame_mut());
-                if let Err(e) = pixels.render() {
-                    log::error!("{}", e);
-                    *control_flow = ControlFlow::Exit;
-                    return;
-                }
-            }
-        }
 
         let processed_all_events = input.update(&event);
         if processed_all_events {
@@ -57,21 +57,44 @@ fn main() -> anyhow::Result<()> {
                 app.destroy();
             }
 
-            if let Some(_) = input.scale_factor_changed() {
-                let size = window.inner_size();
-                app.resize(size);
-                pixels.resize_surface(size.width, size.height);
+            if let Some(scale_factor) = input.scale_factor_changed() {
+                editor.scale_factor(scale_factor)
             }
 
             if let Some(size) = input.window_resized() {
                 app.resize(size);
                 pixels.resize_surface(size.width, size.height);
+                editor.resize(size.width, size.height);
             }
 
             if !destroying {
                 app.update(); // TODO: Update loop timing
                 window.request_redraw();
             }
+        }
+
+        match event {
+            Event::WindowEvent { window_id, event } if window_id == window.id() => {
+                editor.handle_event(&event);
+            }
+            Event::RedrawRequested(window_id) if !destroying && window_id == window.id() => {
+                app.draw(pixels.get_frame_mut());
+                editor.draw(&window);
+
+                let render_result = pixels.render_with(|encoder, render_target, context| {
+                    context.scaling_renderer.render(encoder, render_target);
+                    editor.render(encoder, render_target, context);
+
+                    Ok(())
+                });
+
+                if let Err(e) = render_result {
+                    error!("{}", e);
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+            }
+            _ => {}
         }
     });
 }
@@ -82,7 +105,7 @@ struct App {}
 impl App {
     fn update(&self) {}
 
-    fn render(&self, data: &mut [u8]) {
+    fn draw(&self, data: &mut [u8]) {
         for pixel in data.chunks_exact_mut(4) {
             let rgba = [0x33, 0x33, 0x33, 0xff];
 
