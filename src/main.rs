d4966df8be570f4e441edb712d2bbeecde18b97f
Ashley Morton-Bye <ashley.morton.bye@gmail.com>
2022-10-11 14:40:21 +0100

    Scale pixel buffer to window size when rendering

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use editor::Editor;
 use flexi_logger::Logger;
 use log::error;
 use pixels::{PixelsBuilder, SurfaceTexture};
+use renderer::Renderer;
 use winit::{
     dpi::LogicalSize,
     event::{DeviceEvent, ElementState, Event, KeyboardInput},
@@ -11,6 +12,7 @@ use winit::{
 use winit_input_helper::WinitInputHelper;
 
 mod editor;
+mod renderer;
 
 /* TODO's:
  * - Handle exiting and entering fullscreen
@@ -23,8 +25,9 @@ mod editor;
  *   - Store settings in proper place for each OS
  */
 
-const WIDTH: u32 = 960;
-const HEIGHT: u32 = 540;
+const ASPECT_RATIO: f32 = 16_f32 / 9_f32;
+const WIDTH: u32 = 1024;
+const HEIGHT: u32 = (WIDTH as f32 / ASPECT_RATIO) as u32;
 
 fn main() -> anyhow::Result<()> {
     let max_level = "info";
@@ -38,22 +41,11 @@ fn main() -> anyhow::Result<()> {
         .expect("no monitors found");
 
     let window = {
-        let monitor_size = monitor.size();
-        let (mut width, mut height) = (WIDTH, HEIGHT);
-        loop {
-            if width * 2 <= monitor_size.width && height * 2 <= monitor_size.height {
-                width *= 2;
-                height *= 2;
-            } else {
-                break;
-            }
-        }
-        let size = LogicalSize::new(width as f64, height as f64);
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
         WindowBuilder::new()
             .with_title("Papercut")
             .with_inner_size(size)
             .with_min_inner_size(size)
-            .with_max_inner_size(size)
             .with_position(monitor.position())
             .with_visible(false)
             .build(&event_loop)?
@@ -64,13 +56,17 @@ fn main() -> anyhow::Result<()> {
     let (mut pixels, mut editor) = {
         let size = window.inner_size();
         let scale_factor = window.scale_factor() as f32;
-        let surface_texture = SurfaceTexture::new(size.width, size.height, &window);
-        let pixels = pollster::block_on(
-            PixelsBuilder::new(size.width, size.height, surface_texture).build_async(),
-        )?;
+        let surface_texture = SurfaceTexture::new(WIDTH, HEIGHT, &window);
+        let pixels =
+            pollster::block_on(PixelsBuilder::new(WIDTH, HEIGHT, surface_texture).build_async())?;
         let editor = Editor::new(&event_loop, size.width, size.height, scale_factor, &pixels);
 
         (pixels, editor)
+    };
+
+    let mut renderer = {
+        let size = window.inner_size();
+        Renderer::new(&pixels, WIDTH, HEIGHT, size.width, size.height)
     };
 
     let app = App::default();
@@ -95,6 +91,10 @@ fn main() -> anyhow::Result<()> {
             if let Some(size) = input.window_resized() {
                 app.resize(size);
                 pixels.resize_surface(size.width, size.height);
+
+                let clip_rect = pixels.context().scaling_renderer.clip_rect();
+                renderer.resize(&pixels, clip_rect.2, clip_rect.3, size.width, size.height);
+
                 editor.resize(size.width, size.height);
             }
 
@@ -121,11 +121,19 @@ fn main() -> anyhow::Result<()> {
                 editor.handle_event(&event);
             }
             Event::RedrawRequested(window_id) if !destroying && window_id == window.id() => {
-                app.draw(pixels.get_frame_mut());
+                let mut buffer = Buffer {
+                    width: WIDTH,
+                    height: HEIGHT,
+                    data: pixels.get_frame_mut(),
+                };
+                app.draw(&mut buffer);
                 editor.draw(&window);
 
                 let render_result = pixels.render_with(|encoder, render_target, context| {
-                    context.scaling_renderer.render(encoder, render_target);
+                    let fill_texture = renderer.get_texture_view();
+                    context.scaling_renderer.render(encoder, fill_texture);
+
+                    renderer.render(encoder, render_target);
                     editor.render(encoder, render_target, context);
 
                     Ok(())
@@ -148,15 +156,50 @@ struct App {}
 impl App {
     fn update(&self) {}
 
-    fn draw(&self, data: &mut [u8]) {
-        for pixel in data.chunks_exact_mut(4) {
-            let rgba = [0x33, 0x33, 0x33, 0xff];
-
-            pixel.copy_from_slice(&rgba);
-        }
+    fn draw(&self, buffer: &mut Buffer) {
+        let rgba = [0x33, 0x33, 0x33, 0xff];
+        Gfx::clear(buffer, rgba);
+        Gfx::draw_square(buffer, 50, 500, 10, 10, [0xff, 0x00, 0x00, 0xff]);
     }
 
     fn resize(&self, _size: winit::dpi::PhysicalSize<u32>) {}
 
     fn destroy(&self) {}
+}
+
+struct Buffer<'a> {
+    width: u32,
+    height: u32,
+    data: &'a mut [u8],
+}
+
+struct Gfx {}
+
+impl Gfx {
+    pub fn clear(buffer: &mut Buffer, rgba: [u8; 4]) {
+        for y_ in 0..buffer.height {
+            for x_ in 0..buffer.width {
+                let i = (y_ * buffer.width + x_) as usize * 4;
+                let pixel = &mut buffer.data[i..i + 4];
+                pixel.copy_from_slice(&rgba)
+            }
+        }
+    }
+
+    pub fn draw_square(
+        buffer: &mut Buffer,
+        x: i32,
+        y: i32,
+        width: i32,
+        height: i32,
+        rgba: [u8; 4],
+    ) {
+        for y_ in y..(y + height) {
+            for x_ in x..(x + width) {
+                let i = (y_ * buffer.width as i32 + x_) as usize * 4;
+                let pixel = &mut buffer.data[i..i + 4];
+                pixel.copy_from_slice(&rgba)
+            }
+        }
+    }
 }
